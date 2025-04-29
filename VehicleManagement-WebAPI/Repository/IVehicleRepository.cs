d537b47227ed1b45a164d77ab19370789a452721
DotnetcoreInsights <dotnetcoreinsightshub@gmail.com>
2025-04-29 11:14:45 +0530

    Declared methods in interface repository

--- VehicleManagement-WebAPI/Repository/IVehicleRepository.cs
+++ VehicleManagement-WebAPI/Repository/IVehicleRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace VehicleManagement_WebAPI.Repository
+﻿using VehicleManagement_WebAPI.Models;
+
+namespace VehicleManagement_WebAPI.Repository
 {
     public interface IVehicleRepository
     {
+        IEnumerable<VehicleModel> GetAllVehicles();
+        VehicleModel GetVehicleById(int vehicleId);
+        void AddVehicle(VehicleModel vehicle);
+        bool UpdateVehicle(VehicleModel vehicle);
+        bool DeleteVehicle(int vehicleId);
     }
 }
